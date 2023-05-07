a4a49ce7b4386b3ecbaf7985cbfd9b59582ae592
vdekterev <vlad.decterev@icloud.com>
2023-05-07 11:34:08 +0400

    task9

--- task9-timer/script.js
+++ task9-timer/script.js
@@ -1,4 +1,44 @@
 document.addEventListener('DOMContentLoaded', () => {
+    
+    // Tabs
+    
+    const tabList = document.querySelectorAll('.tabheader__item'),
+        tabListWrapper = document.querySelector('.tabheader__items'),
+        tabPictures = document.querySelectorAll('.tabcontent');
+
+    function hideTabs() {
+        tabPictures.forEach(tab => {
+            tab.style.display = 'none';
+        })
+        tabList.forEach(tab => {
+            tab.classList.remove('tabheader__item_active');
+        })
+    }
+
+    function showTab(index = 0) {
+        const tab = tabListWrapper.children[index];
+        tab.classList.add('tabheader__item_active');
+        tabPictures[index].style.display = '';
+    }
+
+    hideTabs();
+    showTab();
+
+    tabListWrapper.addEventListener('click', (event) => {
+        const tab = event.target;
+        
+        if (tab?.classList.contains('tabheader__item')) {
+            hideTabs();
+            tabList.forEach((element, index) => {
+                if (tab == element) {
+                    showTab(index);
+                };
+            });
+        };
+    }); 
+    
+    // Timer
+
     function timeLeft(deadline) {
         const t = Date.parse(deadline) - Date.parse(new Date());
         return {
@@ -38,5 +78,4 @@ document.addEventListener('DOMContentLoaded', () => {
     }
 
     setTimeLeft('.timer', '2023-05-21 13:48');
-
 });
